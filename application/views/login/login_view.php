e7421ddb41ea29c71622e985a4d7b04a6312668b
Juan Pablo Mayoral <jpmayoral@gmail.com>
2012-08-03 00:03:41 -0300

    Agrego checkbox en login

--- application/views/login/login_view.php
+++ application/views/login/login_view.php
@@ -6,6 +6,9 @@
 			<input type="text" size="20" id="usuario_email" name="usuario_email" placeholder="Ingrese Email" class="span3"/>
 			<label for="usuario_password">Contraseña:</label>
 			<input type="password" size="20" id="usuario_password" name="usuario_password" placeholder="Contraseña" class="span3"/>				
+			<label class="checkbox">
+    			<input type="checkbox"> No cerrar la sesión
+  			</label>
 			<input class="btn" type="submit" value="Ingresar"/>
 		</fieldset>		
 	</form>
