2fd668298338bb2ea9a0d5c6692454342f9cc9ac
Juan Pablo Mayoral <jpmayoral@gmail.com>
2012-08-29 17:06:40 -0300

    modifico carga de js

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -21,16 +21,6 @@
 
 		<!-- Javascript placed at the end of the document so the pages load faster -->
 		<script src="<?=base_url();?>assets/bootstrap/js/jquery.js"></script>
-		<script src="<?=base_url();?>assets/bootstrap/js/bootstrap-transition.js"></script>
-		<script src="<?=base_url();?>assets/bootstrap/js/bootstrap-modal.js"></script>
-		<script src="<?=base_url();?>assets/bootstrap/js/bootstrap-dropdown.js"></script>
-		<script src="<?=base_url();?>assets/bootstrap/js/bootstrap-scrollspy.js"></script>
-		<script src="<?=base_url();?>assets/bootstrap/js/bootstrap-tab.js"></script>
-		<script src="<?=base_url();?>assets/bootstrap/js/bootstrap-tooltip.js"></script>
-		<script src="<?=base_url();?>assets/bootstrap/js/bootstrap-popover.js"></script>
-		<script src="<?=base_url();?>assets/bootstrap/js/bootstrap-button.js"></script>
-		<script src="<?=base_url();?>assets/bootstrap/js/bootstrap-collapse.js"></script>
-		<script src="<?=base_url();?>assets/bootstrap/js/bootstrap-carousel.js"></script>
-		<script src="<?=base_url();?>assets/bootstrap/js/bootstrap-typeahead.js"></script>
+		<script src="<?=base_url();?>assets/bootstrap/js/bootstrap.js"></script>
 	</body>
 </html>
