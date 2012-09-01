a4ca9a4f303aef30b973eb81c244c6303eca3e1f
Juan Pablo Mayoral <jpmayoral@gmail.com>
2012-09-01 15:22:22 -0300

    solucionado el conflicto de jquery entre bootstrap y grocery

--- application/controllers/pagos_controller.php
+++ application/controllers/pagos_controller.php
@@ -2,8 +2,6 @@
 
 class Pagos_Controller extends CI_Controller {
 
-	
-
 	function __construct()
 	{
 		parent::__construct();		
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -17,10 +17,11 @@
 		<?php foreach ($js_files as $file):?>
 			<script src="<?=$file?>"></script>
 		<? endforeach; ?>
+	<? else: ?>
+		<script src="<?=base_url()?>assets/grocery_crud/js/jquery-1.8.0.min.js"></script>
 	<? endif;?>
 
 		<!-- Javascript placed at the end of the document so the pages load faster -->
-		<!--<script src="<?=base_url();?>assets/bootstrap/js/jquery.js"></script>-->
-		<script src="<?=base_url();?>assets/bootstrap/js/bootstrap.js"></script>
+		<script src="<?=base_url()?>assets/bootstrap/js/bootstrap.js"></script>
 	</body>
 </html>
